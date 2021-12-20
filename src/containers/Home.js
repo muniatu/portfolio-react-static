cc3d83b30b2c5ee578f2b5cb81dc9887e51ba7f3
Adrià Compte <muniatu@gmail.com>
2021-12-20 20:22:53 +0100

    Update Home.js

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,9 +1,16 @@
 import React from 'react'
 //
+import Head from 'react-static'
 import logoImg from '../logo.png'
 
 export default () => (
   <div>
+    <Head>
+      <meta charset="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>Adrià Compte | Product Designer and Creative Developer</title>
+      <meta name="description" content="Adrià Compte's Personal Website" />
+    </Head>
     <h1 style={{ textAlign: 'center' }}>Welcome to</h1>
     <img src={logoImg} alt="" />
   </div>
