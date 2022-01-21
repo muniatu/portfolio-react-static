f05176167da98e06e5a07a0fceff471f71c35880
Adrià Compte <muniatu@gmail.com>
2022-01-21 21:23:35 +0100

    bug fixes

---
+++ tailwind.config.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    fontFamily: {
+      sans: ['Inter', 'sans-serif'],
+      serif: ['"PT Serif"', 'serif'],
+    },
+    extend: {
+      fontSize: {
+        '10xl': ['9rem', '1']
+      },
+    },
+  },
+  plugins: [],
+}
